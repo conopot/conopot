bf08eed8ec9ce2766ac7aaa6aff75128e9097cd5
suhwan-cheon <soo7652@naver.com>
2022-09-04 17:29:26 +0900

    feat. 카톡 로그인 - 토큰 유효성 체크 로직 추가

--- lib/screens/user/user_screen.dart
+++ lib/screens/user/user_screen.dart
@@ -78,37 +78,25 @@ class _UserScreenState extends State<UserScreen> {
                       style: TextStyle(color: Colors.white),
                     ),
                     onTap: () async {
-                      if (await isKakaoTalkInstalled()) {
+                      if (await AuthApi.instance.hasToken()) {
                         try {
-                          OAuthToken token =
-                              await UserApi.instance.loginWithKakaoTalk();
-                          print('카카오톡으로 로그인 성공 ${token.accessToken}');
+                          AccessTokenInfo tokenInfo =
+                              await UserApi.instance.accessTokenInfo();
+                          print(
+                              '토큰 유효성 체크 성공 ${tokenInfo.id} ${tokenInfo.expiresIn}');
                         } catch (error) {
-                          print('카카오톡으로 로그인 실패 $error');
+                          if (error is KakaoException &&
+                              error.isInvalidTokenError()) {
+                            print('토큰 만료 $error');
+                          } else {
+                            print('토큰 정보 조회 실패 $error');
+                          }
 
-                          // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-                          // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-                          if (error is PlatformException &&
-                              error.code == 'CANCELED') {
-                            return;
-                          }
-                          // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
-                          try {
-                            OAuthToken token =
-                                await UserApi.instance.loginWithKakaoTalk();
-                            print('카카오톡으로 로그인 성공 ${token.accessToken}');
-                          } catch (error) {
-                            print('카카오계정으로 로그인 실패 $error');
-                          }
+                          loginTry();
                         }
                       } else {
-                        try {
-                          OAuthToken token =
-                              await UserApi.instance.loginWithKakaoTalk();
-                          print('카카오톡으로 로그인 성공 ${token.accessToken}');
-                        } catch (error) {
-                          print('카카오계정으로 로그인 실패 $error');
-                        }
+                        print('발급된 토큰 없음');
+                        loginTry();
                       }
                     },
                   ),
@@ -135,5 +123,36 @@ class _UserScreenState extends State<UserScreen> {
                 ),
               ),
             ));
+  }
+
+  void loginTry() async {
+    if (await isKakaoTalkInstalled()) {
+      try {
+        OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
+        print('카카오톡으로 로그인 성공 ${token.accessToken}');
+      } catch (error) {
+        print('카카오톡으로 로그인 실패 $error');
+
+        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+        if (error is PlatformException && error.code == 'CANCELED') {
+          return;
+        }
+        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+        try {
+          OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
+          print('카카오계정으로 로그인 성공 ${token.accessToken}');
+        } catch (error) {
+          print('카카오계정으로 로그인 실패 $error');
+        }
+      }
+    } else {
+      try {
+        OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
+        print('카카오계정으로 로그인 성공 ${token.accessToken}');
+      } catch (error) {
+        print('카카오계정으로 로그인 실패 $error');
+      }
+    }
   }
 }
