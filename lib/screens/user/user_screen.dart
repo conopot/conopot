9564e1db649b16cd0d7eba68c99df442511cd9d2
suhwan-cheon <soo7652@naver.com>
2022-09-07 16:07:36 +0900

    feat: jwt 토큰 반환 로직 추가

--- lib/screens/user/user_screen.dart
+++ lib/screens/user/user_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:conopot/config/analytics_config.dart';
 import 'package:conopot/config/constants.dart';
 import 'package:conopot/config/size_config.dart';
@@ -8,6 +10,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:provider/provider.dart';
+
+import 'package:http/http.dart' as http;
 
 class UserScreen extends StatefulWidget {
   UserScreen({Key? key}) : super(key: key);
@@ -78,26 +82,28 @@ class _UserScreenState extends State<UserScreen> {
                       style: TextStyle(color: Colors.white),
                     ),
                     onTap: () async {
-                      if (await AuthApi.instance.hasToken()) {
-                        try {
-                          AccessTokenInfo tokenInfo =
-                              await UserApi.instance.accessTokenInfo();
-                          print(
-                              '토큰 유효성 체크 성공 ${tokenInfo.id} ${tokenInfo.expiresIn}');
-                        } catch (error) {
-                          if (error is KakaoException &&
-                              error.isInvalidTokenError()) {
-                            print('토큰 만료 $error');
-                          } else {
-                            print('토큰 정보 조회 실패 $error');
-                          }
+                      //테스트를 위해 일단 무조건 로그인하는 로직
+                      loginTry();
+                      // if (await AuthApi.instance.hasToken()) {
+                      //   try {
+                      //     AccessTokenInfo tokenInfo =
+                      //         await UserApi.instance.accessTokenInfo();
+                      //     print(
+                      //         '토큰 유효성 체크 성공 ${tokenInfo.id} ${tokenInfo.expiresIn}');
+                      //   } catch (error) {
+                      //     if (error is KakaoException &&
+                      //         error.isInvalidTokenError()) {
+                      //       print('토큰 만료 $error');
+                      //     } else {
+                      //       print('토큰 정보 조회 실패 $error');
+                      //     }
 
-                          loginTry();
-                        }
-                      } else {
-                        print('발급된 토큰 없음');
-                        loginTry();
-                      }
+                      //     loginTry();
+                      //   }
+                      // } else {
+                      //   print('발급된 토큰 없음');
+                      //   loginTry();
+                      // }
                     },
                   ),
                 ]),
@@ -130,6 +136,7 @@ class _UserScreenState extends State<UserScreen> {
       try {
         OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
         print('카카오톡으로 로그인 성공 ${token.accessToken}');
+        register(token);
       } catch (error) {
         print('카카오톡으로 로그인 실패 $error');
 
@@ -142,6 +149,7 @@ class _UserScreenState extends State<UserScreen> {
         try {
           OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
           print('카카오계정으로 로그인 성공 ${token.accessToken}');
+          register(token);
         } catch (error) {
           print('카카오계정으로 로그인 실패 $error');
         }
@@ -150,9 +158,25 @@ class _UserScreenState extends State<UserScreen> {
       try {
         OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
         print('카카오계정으로 로그인 성공 ${token.accessToken}');
+        register(token);
       } catch (error) {
         print('카카오계정으로 로그인 실패 $error');
       }
     }
   }
+}
+
+void register(OAuthToken token) async {
+  String url = 'http://10.0.2.2:3000/auth/signin';
+
+  final response = await http.post(Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode({
+        'accessToken': token.accessToken,
+      }));
+
+  //print(response.body);
+  print(response.headers);
 }
